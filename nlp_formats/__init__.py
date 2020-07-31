5c9c154cd75733229b1f201224886c0a24cb59f8
ArneBinder <ArneBinder@users.noreply.github.com>
2020-07-31 17:05:31 +0200

    Update __init__.py

--- nlp_formats/__init__.py
+++ nlp_formats/__init__.py
@@ -1,2 +1,2 @@
-from brat import AbstractBrat, BratConfig
-from germeval2014 import AbstractGermeval2014, Germeval2014Config
+from nlp_formats.brat import AbstractBrat, BratConfig
+from nlp_formats.germeval2014 import AbstractGermeval2014, Germeval2014Config
